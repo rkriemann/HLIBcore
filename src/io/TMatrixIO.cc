416eb9bb4294393858996bd0060bd5224ff1d1fc
Ronald Kriemann <rok@mis.mpg.de>
2021-12-01 14:30:05 +0100

    C-API in HDF5
    - io/TMatrixIO:
      - switched to C-API in HDF5 reading due to API problems in C++ code

--- src/io/TMatrixIO.cc
+++ src/io/TMatrixIO.cc
@@ -1475,6 +1475,94 @@ h5_read_dense ( H5File *             file,
     return  M;
 }
 
+herr_t
+visit_func ( hid_t               /* loc_id */,
+             const char *        name,
+             const H5O_info_t *  info,
+             void *              operator_data )
+{
+    std::string *  dname = static_cast< std::string * >( operator_data );
+    std::string    oname = name;
+
+    if ( oname[0] != '.' )
+    {
+        if ( info->type == H5O_TYPE_GROUP )
+        {
+            // use first name encountered
+            if ( *dname == "" )
+                *dname = oname;
+        }// if
+        else if (( *dname != "" ) && ( info->type == H5O_TYPE_DATASET ))
+        {
+            // reset if not expected value
+            if (( oname != *dname + "/type" ) &&
+                ( oname != *dname + "/value" ))
+                *dname = "";
+        }// if
+        
+        // switch ( info->type )
+        // {
+        //     case H5O_TYPE_GROUP:
+        //         std::cout << oname << "  (Group)" << std::endl;
+        //         break;
+        //     case H5O_TYPE_DATASET:
+        //         std::cout << oname << "  (Dataset)" << std::endl;
+        //         break;
+        //     case H5O_TYPE_NAMED_DATATYPE:
+        //         std::cout << oname << "  (Datatype)" << std::endl;
+        //         break;
+        //     default:
+        //         std::cout << oname << "  (Unknown)" << std::endl;
+        // }// switch
+    }// if
+    
+    return 0;
+}
+
+template < typename value_t >
+const BLAS::Matrix< value_t >
+h5_read_dense_c ( hid_t                file,
+                  const std::string &  /* mname */ )
+{
+    if ( is_complex_type< value_t >::value )
+        HERROR( ERR_CONSISTENCY, "", "" );
+
+    auto  data_name = std::string( "" );
+    auto  status    = H5Ovisit( file, H5_INDEX_NAME, H5_ITER_INC, visit_func, & data_name );
+
+    if ( status != 0 )
+        return BLAS::Matrix< value_t >( 0, 0 );
+
+    // std::cout << "found: " << data_name << std::endl;
+    
+    // check if any compatible type found
+    if ( data_name == "" )
+        return BLAS::Matrix< value_t >( 0, 0 );
+
+    data_name = data_name + "/value";
+    
+    auto  dataset   = H5Dopen( file, data_name.c_str(), H5P_DEFAULT );
+    auto  dataspace = H5Dget_space( dataset );
+    auto  ndims     = H5Sget_simple_extent_ndims( dataspace );
+    auto  dims      = std::vector< hsize_t >( ndims );
+
+    if ( ndims != 2 )
+        std::cout << "not a matrix" << std::endl;
+                                                
+    H5Sget_simple_extent_dims( dataspace, dims.data(), nullptr );
+    
+    // std::cout << dims[0] << " × " << dims[1] << std::endl;
+
+    auto  M = BLAS::Matrix< value_t >( dims[0], dims[1] );
+
+    status = H5Dread( dataset, H5T_NATIVE_DOUBLE, H5S_ALL, H5S_ALL, H5P_DEFAULT, M.data() );
+    
+    if ( status != 0 )
+        return BLAS::Matrix< value_t >( 0, 0 );
+    
+    return  M;
+}
+
 }// namespace anonymous
 
 #endif
@@ -1586,9 +1674,8 @@ THDF5MatrixIO::read  ( const std::string &  filename ) const
 {
     try
     {
-        H5File  file( filename, H5F_ACC_RDONLY );
-
-        auto  M = h5_read_dense< real >( & file, "" );
+        auto  file = H5Fopen( filename.c_str(), H5F_ACC_RDONLY, H5P_DEFAULT );
+        auto  M    = h5_read_dense_c< real >( file, "" );
 
         return std::make_unique< TDenseMatrix >( is( 0, M.nrows()-1 ), is( 0, M.ncols()-1 ), std::move( M ) );
     }// try
