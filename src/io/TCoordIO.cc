0ec8dd757b6ed01df2e06487747b6bcf8150e57f
Ronald Kriemann <rok@mis.mpg.de>
2024-03-01 08:40:40 +0100

    fixed inclusion of boost::phoenix

--- src/io/TCoordIO.cc
+++ src/io/TCoordIO.cc
@@ -12,8 +12,8 @@
 #include <boost/filesystem.hpp>
 #include <boost/format.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
 #include <boost/algorithm/string/trim.hpp>
 
 #include "baseio.hh"
--- src/io/TVectorIO.cc
+++ src/io/TVectorIO.cc
@@ -13,8 +13,8 @@
 #include <boost/format.hpp>
 #include <boost/algorithm/string/case_conv.hpp>
 #include <boost/spirit/include/qi.hpp>
-#include <boost/spirit/include/phoenix_core.hpp>
-#include <boost/spirit/include/phoenix_operator.hpp>
+#include <boost/phoenix/core.hpp>
+#include <boost/phoenix/operator.hpp>
 
 #include "hpro/vector/TScalarVector.hh"
 #include "hpro/base/traits.hh"
