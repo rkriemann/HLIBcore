f067d75ed94389dc5fed978369971332e514572b
Ronald Kriemann <rok@mis.mpg.de>
2024-10-25 20:15:47 +0200

    baseio: using C++ cstdint instead of boost::cstdint

--- src/include/baseio.hh
+++ src/include/baseio.hh
@@ -11,8 +11,7 @@
 #include <istream>
 #include <ostream>
 #include <string>
-
-#include <boost/cstdint.hpp>
+#include <cstdint>
 
 #include "hpro/base/types.hh"
 #include "hpro/base/traits.hh"
@@ -26,17 +25,17 @@ namespace Hpro
 //
 //////////////////////////////////////////////////////////////
 
-using  int8_t   = boost::int8_t;
-using  uint8_t  = boost::uint8_t;
+using  int8_t   = std::int8_t;
+using  uint8_t  = std::uint8_t;
                 
-using  int16_t  = boost::int16_t;
-using  uint16_t = boost::uint16_t;
+using  int16_t  = std::int16_t;
+using  uint16_t = std::uint16_t;
                 
-using  int32_t  = boost::int32_t;
-using  uint32_t = boost::uint32_t;
+using  int32_t  = std::int32_t;
+using  uint32_t = std::uint32_t;
                 
-using  int64_t  = boost::int64_t;
-using  uint64_t = boost::uint64_t;
+using  int64_t  = std::int64_t;
+using  uint64_t = std::uint64_t;
 
 ///////////////////////////////////////////////////
 // 
